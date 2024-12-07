5651eb1c8dd701ee63aaec087c7ce98f5af41c11
MaximoBueno <maxhugobueno@gmail.com>
2024-12-06 23:47:30 -0500

    add controller

---
+++ web.bueno.crm.lia/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.bueno.crm.lia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+
+    }
+}
